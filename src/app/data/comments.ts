6188e9c98fac9ca2e326656af893385b9643bdd1
augustorib <augustorifra@gmail.com>
2024-01-14 20:20:45 -0300

    feat: add comments fake database

---
+++ src/app/data/comments.ts
@@ -0,0 +1,8 @@
+export const comments = [
+    {
+      "id":"1",
+      "profileImg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkgJA5mgmZhcL2u07ljnXuxV6xsS-R9GTzww&usqp=CAU",
+      "comment": "awesome  trailer!!!",
+      "movieId" : "8ugaeA-nMTc"
+    }
+  ]
