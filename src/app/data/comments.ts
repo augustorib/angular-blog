a651b95fc520f41a83388543c7c208d2cff20225
augustorib <augustorifra@gmail.com>
2024-01-15 04:38:27 -0300

    feat: add some data to comments.ts and a shaped type "TypeComments"

--- src/app/data/comments.ts
+++ src/app/data/comments.ts
@@ -1,8 +1,31 @@
+export type TypeComments = {
+  id: string; 
+  profileImg: string;
+  comment: string;
+  movieId: string;
+  commentPassedTime: string;
+}
+
 export const comments = [
     {
       "id":"1",
+      "movieId" : "8ugaeA-nMTc",
       "profileImg": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkgJA5mgmZhcL2u07ljnXuxV6xsS-R9GTzww&usqp=CAU",
       "comment": "awesome  trailer!!!",
-      "movieId" : "8ugaeA-nMTc"
-    }
+      "commentPassedTime": "10 min"
+    },
+    {
+      "id":"2",
+      "movieId" : "8ugaeA-nMTc",
+      "profileImg": "https://t.ctcdn.com.br/gALBpOOLuD2ysbABT4c6AL71ynI=/1244x700/smart/i759818.jpeg",
+      "comment": "I played very well in this film, I deserve an oscar! kkkk",
+      "commentPassedTime": "1 month"
+    },
+    {
+      "id":"3",
+      "movieId" : "8ugaeA-nMTc",
+      "profileImg": "https://i.ytimg.com/vi/ltRxlrAENXk/sddefault.jpg",
+      "comment": "my film was better than that, cheers! kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk",
+      "commentPassedTime": "3 month"
+    },
   ]
